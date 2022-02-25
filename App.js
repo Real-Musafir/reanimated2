2f77ea72250974374a0720f4f0b779b5403c8177
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-25 19:27:53 +0600

    implement reanimatedStyle

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
 } from "react-native-reanimated";
 import "react-native-gesture-handler";
 
 const SIZE = 100.0;
 
 export default function App() {
-  const progress = useSharedValue(0);
+  const progress = useSharedValue(1);
 
   const reanimatedStyle = useAnimatedStyle(() => {
     return {
@@ -18,10 +19,17 @@ export default function App() {
     };
   });
 
+  useEffect(() => {
+    progress.value = withTiming(0, { duration: 5000 });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Animated.View
-        style={{ height: SIZE, width: SIZE, backgroundColor: "blue" }}
+        style={[
+          { height: SIZE, width: SIZE, backgroundColor: "blue" },
+          reanimatedStyle,
+        ]}
       />
     </View>
   );
